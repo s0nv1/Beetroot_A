9b9de5bf2754232a99051bb572c87e0c8dd345a2
s0nv1 <sapko1342@gmail.com>
2022-02-08 22:33:07 +0200

    beetroot

---
+++ Homework/hw_08.02.22.py
@@ -0,0 +1,19 @@
+# Output text: 'Hello world'.
+print('Hello world')
+
+# Output text: 'Hello', 'world'. With separation = ', '.
+print('Hello', 'world', sep=', ')
+
+# Output text: 'Hello', 'world'. With separation = ', ',ending = '!'.
+print('Hello', 'world', sep=', ', end='!')
+
+# Output letter 'O' made from symbol '#' with help ''''''.
+print('''#########
+#       #
+#       #
+#       #
+#########''')
+
+# Output letter 'H' made from symbol '#' with help '\n' and '\t'.
+# ps. first '\n' for
+print('\n#\t\t#\n#\t\t#\n#########\n#\t\t#\n#\t\t#')
