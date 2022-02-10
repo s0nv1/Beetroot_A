dc9adc404f3081ae24cc2605a1f2b8c7ffbf63d3
s0nv1 <sapko1342@gmail.com>
2022-02-10 23:23:36 +0200

    hw3

---
+++ Homework/hw_12.02.22.py
@@ -0,0 +1,26 @@
+# Task 1
+# String manipulation.
+text = input()
+if len(text) >= 2:
+    print(text[:2] + text[-2:])
+else:
+    print('Empty String')
+
+# Task 2
+# The valid phone number program.
+while True:
+    phone_number = input('Write phone number: ')
+    if phone_number.isdigit() and len(phone_number) == 10:
+        print('Right phone number')
+        break
+    else:
+        print('Not correct phone number, try again.')
+
+# Task 3
+# The name check.
+my_name = 'ihor'
+name = input('Write your name: ').lower()
+if name == my_name:
+    print(True)
+else:
+    print(False)
