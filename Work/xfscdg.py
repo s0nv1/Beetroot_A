0e42868d7d9ecf35a79c2832d0d3841ee9626bde
s0nv1 <sapko1342@gmail.com>
2022-02-15 23:46:17 +0200

    hw

--- Work/xfscdg.py
+++ Work/xfscdg.py
@@ -1,20 +1,8 @@
-import random
-
-list_num = [random.randint(1, 100) for i in range(10)]
-count = 0
-max_digit = 0
-while count != len(list_num):
-    if list_num[count] > max_digit:
-        max_digit = list_num[count]
-    count += 1
-else:
-    print(max_digit)
-
-# for loop
-for i in list_num:
-    if i > max_digit:
-        max_digit = i
-print(max_digit)
-# max()
-print(max(list_num))
+num_from_1_to_100 = [i for i in range(1, 101)]
+new_list = []
+for i in range(0, len(num_from_1_to_100), 7):
+    if num_from_1_to_100[i] % 5 != 0:
+        new_list.append(num_from_1_to_100[i] - 1)
+del new_list[0]
+print(*new_list)
 
