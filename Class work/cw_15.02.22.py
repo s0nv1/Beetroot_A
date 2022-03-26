3c28b77085d19273714b9599fe7b3fbe4002e9a9
s0nv1 <sapko1342@gmail.com>
2022-03-26 17:31:04 +0200

    1

--- Class work/cw_15.02.22.py
+++ Class work/cw_15.02.22.py
@@ -1,4 +1,4 @@
-#1. Write a Python program that accepts a string and calculate the number of digits,
+# 1. Write a Python program that accepts a string and calculate the number of digits,
 # letters in the input string
 s = input()
 digits = 0
@@ -11,7 +11,7 @@ for i in s:
 else:
     print(f'Digits: {digits}\nLetters: {letters}')
 
-#2. Generate 5 random integers between 10 and 1000 which is divisible by 7
+# 2. Generate 5 random integers between 10 and 1000 which is divisible by 7
 import random
 
 count = 0
@@ -21,13 +21,13 @@ while count != 5:
         print(num)
         count += 1
 
-#3. Create a program that reads from the user input int and prints back the square root of this number
+# 3. Create a program that reads from the user input int and prints back the square root of this number
 from math import sqrt
 
 num = int(input())
 print(sqrt(num))
 
-#4.Write a Python program that prints all the numbers from 0 to 6 except 3 and 6.
+# 4.Write a Python program that prints all the numbers from 0 to 6 except 3 and 6.
 # Note : Use 'continue' statement.
 
 for i in range(0, 7):
@@ -35,7 +35,7 @@ for i in range(0, 7):
         continue
     else:
         print(i, end=' ')
-#5. Write a program that takes an integer from user input,
+# 5. Write a program that takes an integer from user input,
 # stores it in variable n and prints an n-by-n table such that
 # there is an `*` in row `i` and column `j` if the greatest common divisor of `i` and `j`
 # is 1 (i and j are relatively prime) and a space character in that position otherwise
@@ -45,18 +45,18 @@ num = int(input())
 for i in range(1, num + 1):
     print()
     for j in range(1, num + 1):
-        if gcd(i,j) == 1:
+        if gcd(i, j) == 1:
             print('*', end='')
         else:
             print(' ', end='')
 
-#6. Write a Python program to convert temperatures to and from celsius, Fahrenheit.
+# 6. Write a Python program to convert temperatures to and from celsius, Fahrenheit.
 # Formula : c/5 = f-32/9 [ where c = temperature in celsius and f = temperature in Fahrenheit
 
 tempC_or_F = int(input('Celsius = 1, Fahrenheit = 2.\nWhat temperature do you want to change to?: '))
 if tempC_or_F == 1:
     faren = int(input('Input Fahrenheit temperature: '))
-    print(f'Celsius temperature: {(5/9)*(faren-32)}')
+    print(f'Celsius temperature: {(5 / 9) * (faren - 32)}')
 else:
     cels = int(input('Input Celsius temperature: '))
-    print(f'Fahrenheit temperature: {((cels - 32)/1.8)}')
+    print(f'Fahrenheit temperature: {((cels - 32) / 1.8)}')
--- Homework/hw_24.02.22.py
+++ Homework/hw_24.02.22.py
@@ -8,18 +8,20 @@ def opps():
 def key_error():
     try:
         opps()
-    except KeyError:
+    except IndexError:
         print('KeyError')
 
 
 key_error()
+
+
 # Task 2
 
 
 def a_b():
     a, b = input(), input()
     try:
-        return int(a)**2 / int(b)
+        return int(a) ** 2 / int(b)
     except ValueError:
         print('ValueError')
     except ZeroDivisionError:
@@ -27,3 +29,54 @@ def a_b():
 
 
 print(a_b())
+
+# Task 3
+# TV controller
+
+CHANNELS = ["BBC", "Discovery", "TV1000"]
+
+
+class TVController:
+    pass
+
+    def __init__(self, channels):
+        self.channels = channels
+        self.channel_online = 0
+
+    def first_channel(self):
+        self.channel_online = 0
+        return self.channels[self.channel_online]
+
+    def last_channel(self):
+        self.channel_online = 2
+        return self.channels[self.channel_online]
+
+    def turn_channel(self, n):
+        self.channel_online = n - 1
+        return self.channels[self.channel_online]
+
+    def next_channel(self):
+        self.channel_online = self.channel_online + 1 if self.channel_online < 2 else 0
+        return self.channels[self.channel_online]
+
+    def previous_channel(self):
+        self.channel_online = self.channel_online - 1 if self.channel_online > 0 else 2
+        return self.channels[self.channel_online]
+
+    def current_channel(self):
+        return self.channels[self.channel_online]
+
+    def is_exist(self, *args):
+        return 'YES' if type(args[0]) == int and 1 <= args[0] <= 3 else 'YES' if type(
+            args[0]) == str and args[0] in self.channels else 'NO'
+
+
+controller = TVController(CHANNELS)
+
+print(controller.first_channel())
+print(controller.last_channel())
+print(controller.turn_channel(1))
+print(controller.next_channel())
+print(controller.previous_channel())
+print(controller.current_channel())
+print(controller.is_exist('2'))
