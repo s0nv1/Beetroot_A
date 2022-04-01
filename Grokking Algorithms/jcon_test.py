e9adff7eec02c7f59aff5b2cfce2801824c585ff
s0nv1 <sapko1342@gmail.com>
2022-04-01 17:46:39 +0300

    1

--- Grokking Algorithms/jcon_test.py
+++
@@ -1,6 +0,0 @@
-import json
-
-with open('dect.json') as file_obj:
-    dect = json.load(file_obj)
-for k, v in dect.items():
-    print(k, ':', v)
--- Grokking Algorithms/json_file.py
+++
@@ -1,9 +0,0 @@
-import json
-
-dect = {}
-dect['name'] = 'Ihor'
-dect['age'] = 22
-dect['city'] = 'Kherson'
-
-with open('dect.json', 'w') as file_obj:
-    json.dump(dect, file_obj)
